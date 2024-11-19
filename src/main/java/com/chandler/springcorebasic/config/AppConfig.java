9b78b0cbb7c06f66fb4575ef2392b3d601478bc3
chandler <parksm0529@gmail.com>
2024-11-19 14:50:16 +0900

    refactor) Remove deplicated

--- src/main/java/com/chandler/springcorebasic/config/AppConfig.java
+++ src/main/java/com/chandler/springcorebasic/config/AppConfig.java
@@ -1,8 +1,10 @@
 package com.chandler.springcorebasic.config;
 
+import com.chandler.springcorebasic.member.MemberRepository;
 import com.chandler.springcorebasic.member.MemberService;
 import com.chandler.springcorebasic.member.MemberServiceImpl;
 import com.chandler.springcorebasic.member.MemoryMemberRepository;
+import com.chandler.springcorebasic.order.DiscountPolicy;
 import com.chandler.springcorebasic.order.FixDiscountPolicy;
 import com.chandler.springcorebasic.order.OrderService;
 import com.chandler.springcorebasic.order.OrderServiceImpl;
@@ -10,11 +12,19 @@ import com.chandler.springcorebasic.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }

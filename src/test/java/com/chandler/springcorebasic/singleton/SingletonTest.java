2643d18ad58ac90d867696f3bb1b628cf0dad6b1
chandler <parksm0529@gmail.com>
2024-11-21 12:42:17 +0900

    test) Auto configuration with a singleton in spring

--- src/test/java/com/chandler/springcorebasic/singleton/SingletonTest.java
+++ src/test/java/com/chandler/springcorebasic/singleton/SingletonTest.java
@@ -2,12 +2,18 @@ package com.chandler.springcorebasic.singleton;
 
 import com.chandler.springcorebasic.config.AppConfig;
 import com.chandler.springcorebasic.member.MemberRepository;
+import com.chandler.springcorebasic.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class SingletonTest {
+
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너 조회시, 인스턴스가 매번 새로 생성됩니다")
@@ -26,6 +32,18 @@ public class SingletonTest {
         SingletonService instance2 = SingletonService.getInstance();
 
         assertSame(instance1, instance2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너 싱글톤")
+    void springContainer_singleton() {
+        MemberService memberService1 = ac.getBean("memberService",MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService",MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertSame(memberService1, memberService2);
     }
 
 }

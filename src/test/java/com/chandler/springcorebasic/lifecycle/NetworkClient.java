ec32b6031fd82e85c4d0f8247309511df02c1155
chandler <parksm0529@gmail.com>
2024-11-23 07:07:49 +0900

    test) Introduction of bean lifecyle

---
+++ src/test/java/com/chandler/springcorebasic/lifecycle/NetworkClient.java
@@ -0,0 +1,30 @@
+package com.chandler.springcorebasic.lifecycle;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + this.url);
+        connect();
+        call("초기화 메세지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // Service start
+    public void connect() {
+        System.out.println("connect, url = " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call : " + url + ", message = " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("close, url = " + url);
+    }
+
+}

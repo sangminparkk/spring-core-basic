f897974969b06c620041f875737db66c80c272f7
chandler <parksm0529@gmail.com>
2024-11-22 14:46:03 +0900

    refactor) Lombok

--- src/main/java/com/chandler/springcorebasic/member/MemberServiceImpl.java
+++ src/main/java/com/chandler/springcorebasic/member/MemberServiceImpl.java
@@ -1,17 +1,13 @@
 package com.chandler.springcorebasic.member;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService{ //TODO: 관례로 구현체가 1개일 경우 "interface명"에 postfix로 impl을 해줌
 
     private final MemberRepository memberRepository;
-
-    @Autowired
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     @Override
     public void join(Member member) {
--- src/main/java/com/chandler/springcorebasic/order/OrderServiceImpl.java
+++ src/main/java/com/chandler/springcorebasic/order/OrderServiceImpl.java
@@ -2,20 +2,15 @@ package com.chandler.springcorebasic.order;
 
 import com.chandler.springcorebasic.member.Member;
 import com.chandler.springcorebasic.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
